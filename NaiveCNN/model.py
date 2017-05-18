5440e43c71036818d2e29d70f90f93f948a1281c
Kankroc <github@belval.org>
2017-05-18 19:36:05 -0400

    Minor changes to max pool function

--- NaiveCNN/model.py
+++ NaiveCNN/model.py
@@ -39,15 +39,30 @@ class ConvolutionalNet():
                 stride -> The applied translation at each step
                 padding -> The padding (padding with 0 so the last column isn't left out)
         """
-        
+
+        if padding >= stride:
+            padding = size 
+
         inp_sp = np.shape(inputs)
         # We reshape it so every filter is considered an image.
         # For example, 10 x 4 x 32 x 32 -> 40 x 1 x 32 x 32
-        reshaped = np.reshape(inputs, 1, (inp_sp[0] * inp_sp[1], inp_sp[2], inp_sp[3]))
-        # This will reshape the previously reshaped input to (if the size is 2) 4 x 
+        reshaped = np.reshape(inputs, (inp_sp[0] * inp_sp[1], 1, inp_sp[2], inp_sp[3]))
+        # This will reshape the previously reshaped input to (if the size is 2) 4 x 10240
         tile_col = im2col_indices(reshaped, size, size, stride=stride, padding=padding)
+        # We take the max of each column
+        max_ids = np.argmax(tile_col, axis=0)
+        # We get the resulting 1 x 10240 vector
+        result = tile_col[max_ids, range(max_ids.size)]
+        # Reshape to get matrices
+        # 2,2,0 -> 28 / 2 = 14
+        # 2,2,1 -> 28 / 2 = 14
+        # 2,3,0 -> 28 / 3 = 9
+        # 2,3,1 -> 29 / 3 = 9
+        # 2,3,1 -> 30 / 3 = 10
+        result = np.reshape(result, )
 
-        
+        # Make it from 16 x 16 x 10 x 4 to 10 x 4 x 16 x 16 
+        return np.reshape(result, (2, 3, 0, 1))
 
     @staticmethod
     def _avg_pool(inputs, size, string, padding):
